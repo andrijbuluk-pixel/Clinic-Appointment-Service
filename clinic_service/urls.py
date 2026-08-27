6eaf80a076c041f370bb70596cc9ae941c04d90b
Andriy <andrijbuluk@gmail.com>
2026-08-27 20:12:15 +0200

    Changed routes to applications

--- clinic_service/urls.py
+++ clinic_service/urls.py
@@ -18,14 +18,26 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView
+)
+from rest_framework.routers import DefaultRouter
+
+from specializations_service.views import SpecializationViewSet
+from doctors_and_slots_service.views import DoctorViewSet
+
+router = DefaultRouter()
+
+router.register("specializations", SpecializationViewSet)
+router.register("doctors", DoctorViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("specializations_service.urls")),
+    path("api/", include(router.urls)),
 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    # Optional UI:
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
--- specializations_service/urls.py
+++ specializations_service/urls.py
@@ -1,10 +1,2 @@
-from rest_framework.routers import DefaultRouter
-
-from specializations_service.views import SpecializationViewSet
 
 app_name = 'specializations_service'
-
-router = DefaultRouter()
-router.register("specializations", SpecializationViewSet)
-
-urlpatterns = router.urls
