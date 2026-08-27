b818fec5aad846a96ffc9c7ac34a74fdad2f16df
Andriy <andrijbuluk@gmail.com>
2026-08-27 14:55:16 +0200

    Added models for Specialization and overridden save for line (code)

--- specializations_service/models.py
+++ specializations_service/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Specialization(models.Model):
+
+    name = models.CharField(max_length=255, unique=True)
+    code = models.SlugField(max_length=255, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self):
+        if not self.code:
+            self.code = slugify(self.name, allow_unicode=True)
+
+        super().save(*args, **kwargs)
