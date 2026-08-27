7255438a56ea7433bcb0fa22797c110422d2db4f
Andriy <andrijbuluk@gmail.com>
2026-08-27 20:18:16 +0200

    serializers Doctor

---
+++ doctors_and_slots_service/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+from doctors_and_slots_service.models import Doctor
+
+
+class DoctorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Doctor
+        fields = (
+            "first_name",
+            "last_name",
+            "specializations",
+            "price_per_visit"
+        )
