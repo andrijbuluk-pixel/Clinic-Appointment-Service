f4f51a599dcc499efef1be7e87536f8e604399d1
Andriy <andrijbuluk@gmail.com>
2026-08-27 16:14:42 +0200

    Added for line (code) blank=True

--- specializations_service/models.py
+++ specializations_service/models.py
@@ -5,7 +5,7 @@ from slugify import slugify
 class Specialization(models.Model):
 
     name = models.CharField(max_length=255, unique=True)
-    code = models.SlugField(max_length=255, unique=True)
+    code = models.SlugField(max_length=255, unique=True, blank=True)
     description = models.TextField(null=True, blank=True)
 
     def __str__(self):
