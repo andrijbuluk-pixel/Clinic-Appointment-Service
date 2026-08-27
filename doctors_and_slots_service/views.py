b430e178260a6e373f18d43bae37b851879559b4
Andriy <andrijbuluk@gmail.com>
2026-08-27 20:33:58 +0200

    filter on id and specializations

--- doctors_and_slots_service/views.py
+++ doctors_and_slots_service/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 
 from doctors_and_slots_service.models import Doctor
 from doctors_and_slots_service.serializers import DoctorSerializer
@@ -7,3 +8,4 @@ from doctors_and_slots_service.serializers import DoctorSerializer
 class DoctorViewSet(viewsets.ModelViewSet):
     serializer_class = DoctorSerializer
     queryset = Doctor.objects.all()
+    filterset_fields = ["id", "specializations"]
