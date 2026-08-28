017c1a5490c8505c96170630d57f0325121d7f35
Andriy <andrijbuluk@gmail.com>
2026-08-28 18:55:22 +0200

    Added serializers for slots

--- doctors_and_slots_service/serializers.py
+++ doctors_and_slots_service/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from doctors_and_slots_service.models import Doctor
+from doctors_and_slots_service.models import Doctor, DoctorSlot
 
 
 class DoctorSerializer(serializers.ModelSerializer):
@@ -12,4 +12,17 @@ class DoctorSerializer(serializers.ModelSerializer):
             "last_name",
             "specializations",
             "price_per_visit"
+        )
+
+
+class DoctorSlotSerializer(serializers.ModelSerializer):
+    start = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
+    end = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
+
+    class Meta:
+        model = DoctorSlot
+        fields = (
+            "doctor",
+            "start",
+            "end"
         )
