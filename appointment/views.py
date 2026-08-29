28721e785210f9fcc68ed0d52006f371ae9cdfab
Andriy <andrijbuluk@gmail.com>
2026-08-29 20:22:35 +0200

    Create a link to appointments with a view

--- appointment/views.py
+++ appointment/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from appointment.models import Appointment
+from appointment.serizlizers import AppointmentSerializer
+
+
+class AppointmentView(viewsets.ModelViewSet):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
