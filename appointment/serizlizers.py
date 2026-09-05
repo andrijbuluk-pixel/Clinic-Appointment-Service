27267405bc25ec27f76d9e0fc73c0fd11c453eb9
Andriy <andrijbuluk@gmail.com>
2026-09-05 17:28:07 +0200

    Update serializers

--- appointment/serizlizers.py
+++ appointment/serizlizers.py
@@ -18,22 +18,17 @@ class AppointmentSerializer(serializers.ModelSerializer):
 
         read_only_fields = ("id", "completed_at")
 
-    class AppointmentSerializer(serializers.ModelSerializer):
-        class Meta:
-            model = Appointment
-            fields = "__all__"  # Або твій список полів
+    def validate(self, attrs):
+        doctor_slot = attrs.get('doctor_slot')
 
-        def validate(self, attrs):
-            doctor_slot = attrs.get('doctor_slot')
+        existing_appointments = Appointment.objects.filter(doctor_slot=doctor_slot)
 
-            existing_appointments = Appointment.objects.filter(doctor_slot=doctor_slot)
+        if self.instance:
+            existing_appointments = existing_appointments.exclude(pk=self.instance.pk)
 
-            if self.instance:
-                existing_appointments = existing_appointments.exclude(pk=self.instance.pk)
+        if existing_appointments.exists():
+            raise serializers.ValidationError({
+                "doctor_slot": "This appointment already exists"
+            })
 
-            if existing_appointments.exists():
-                raise serializers.ValidationError({
-                    "doctor_slot": "This appointment already exists"
-                })
-
-            return attrs
+        return attrs
